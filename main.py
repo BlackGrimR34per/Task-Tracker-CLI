910fe2264e768357ad0c506ca148323aeaa6bfc7
Yohen Sheraun <ysheraun20@yahoo.com>
2025-07-16 22:39:01 +0800

    Temp

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+
+# def handle_user_input(user_input):
+#
+#
+# def task_cli():
+#     user_input = input("task-cli ")
+#     handle_user_input(user_input)
+
+if __name__ == '__main__':
+    task_cli()
